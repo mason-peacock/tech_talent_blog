2dfad0cd34db66843470dff3b4c97afda89edcb0
Mason Peacock <masecock@gmail.com>
2019-09-25 10:51:38 -0400

    initial commit

---
+++ app/controllers/practice_controller.rb
@@ -0,0 +1,13 @@
+class PracticeController < ApplicationController
+  def index
+  	    # we're not using any variables in the view
+        # so nothing to setup in here...yet...
+  end
+
+  def about
+  	# here we'll pull from the query string
+    # and save that value in an instance variable
+    @color = params[:color]
+
+  end
+end
